0be971e9fd76a4dd29325b70f7472e12707a3caa
Keita Ishibashi <fromjpn@gmail.com>
2014-06-26 20:38:57 +0900

    first commit

---
+++ Assets/rotY.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+  transform.Rotate(Vector3.up * 10 * Time.deltaTime, Space.World);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+http://peroon.github.io/crazybump
